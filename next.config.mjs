a05a713e49438f336156369816eb6c0485dae550
rafenri22 <117822812+rafenri22@users.noreply.github.com>
2024-11-03 20:23:18 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default nextConfig;
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
+  };
+  
+  export default nextConfig;
+  
